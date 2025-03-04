80b410f89840ac556d9292cfb33e6cb0860a769d
Tigran <irontigf@mail.ru>
2025-03-04 15:36:10 +0300

    check

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import * as styles from './App.module.scss'
 
 const App: React.FC = () => {
-  return <h1 className={styles.main}>Hello, TypeScript and React!</h1>
+  return <h1 className={styles.main}>Hello, TypeScript and Reactfd!</h1>
 }
 
 export default App
