9ae9bd3b0923910ce3b0e4d037f832bdc0c63775
Tigran <irontigf@mail.ru>
2025-03-04 17:32:44 +0300

    Добавден react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,49 @@ import React, { useCallback, useEffect, useState } from 'react'
 
 import * as styles from './App.module.scss'
 import Increase from './components/Increase/Increase'
+import { useQuery } from '@tanstack/react-query'
 
 const App = () => {
   const [count, setCount] = useState<number>(0)
 
-  const getGamesList = async () => {
-    const res = await fetch(
-      'https://free-to-play-games-database.p.rapidapi.com/api/games',
-      {
+  // const getGamesList = async () => {
+  //   const res = await fetch(
+  //     'https://free-to-play-games-database.p.rapidapi.com/api/games',
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         'x-rapidapi-key':
+  //           'f55c063c32msh976cb9fe35a9e33p1ecebfjsncb5e72197947',
+  //         'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
+  //       },
+  //     }
+  //   )
+  //   const result = await res
+  //   console.log(result)
+  // }
+
+  // useEffect(() => {
+  //   getGamesList()
+  // }, [])
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', {
         method: 'GET',
         headers: {
           'x-rapidapi-key':
             'f55c063c32msh976cb9fe35a9e33p1ecebfjsncb5e72197947',
           'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
         },
-      }
-    )
-    const result = await res
-    console.log(result)
-  }
+      }).then((res) => res.json()),
+  })
 
-  useEffect(() => {
-    getGamesList()
-  }, [])
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  console.log(data)
 
   return (
     <>
